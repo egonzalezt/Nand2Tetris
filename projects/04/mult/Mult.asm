c929df8c087574d32619c54b9979117ad61df505
egonzalezt <egonzalezt@eafit.edu.co>
2021-09-02 18:58:39 -0500

    Mult operation 50/100
    need to add some exceptions

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,33 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@inc
+M=0
+
+@R2
+M=0
+
+
+
+(LOOP)
+    @inc
+    D=M
+    @R1
+    D=D-M //D=INC-RAM[1]
+    @END
+    D;JGE   //if inc >= R1 END loop
+    
+    @R0
+    D=M
+    @R2 
+    M=M+D //R2 = R2+R0
+
+    @inc
+    M=M+1   //INC= INC + 1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
+    
